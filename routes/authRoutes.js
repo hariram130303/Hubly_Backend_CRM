f28ea3ed9bd31c65160a08890f775947d45d50eb
Hari Ram <tmhariram@gmail.com>
2025-11-30 22:57:23 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
-// backend/routes/authRoutes.js
 import express from "express";
 import { signup, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
+// USER SIGNUP
 router.post("/signup", signup);
+
+// USER LOGIN
 router.post("/login", login);
 
 export default router;
